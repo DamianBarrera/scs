fa575549a74b7b280842588cda1cbbca7c351e86
Damian Barrera <>
2022-03-12 16:02:44 -0300

    Agregada Euge

---
+++ euge/script.js
@@ -0,0 +1,74 @@
+const container = document.querySelector('.gallery');
+const fotos = document.querySelector('.gallery img');
+const modal = document.querySelector('.modal'); 
+const close = document.querySelector('.close'); 
+const prev = document.querySelector('.prev'); 
+const next = document.querySelector('.next'); 
+let contadorImg = undefined ;
+
+const euge = ["euge1.jpeg","euge2.jpeg","euge3.jpeg","euge4.jpeg","euge5.jpeg"]
+
+
+
+
+
+// *************************
+euge.forEach( A => crearCard(A) );
+container.addEventListener('click', abrirModal);
+close.addEventListener('click',cerrarModal);
+
+ 
+
+// *********funciones*********
+
+
+function crearCard( param ){
+    const fragment = document.createDocumentFragment();
+    const card = document.createElement('DIV');
+    card.classList.add('card');
+    const image =document.createElement('IMG');
+    image.src=param;
+    image.classList.add('pic')
+    card.appendChild(image);
+    fragment.appendChild(card)
+    container.appendChild(fragment)
+    
+}
+
+function abrirModal (e) {
+    if(e.target.classList.contains('pic')){
+        modal.style.display="block";
+        crearImagen(e);
+        container.addEventListener('click',(e)=>{
+            contadorImg = Array.from(fotos).indexOf(e.target)
+             
+        }) 
+       
+    }
+      
+      
+}
+function crearImagen(e){
+     document.querySelector('.modal img').src = e.target.src;
+     
+}
+
+function cerrarModal(e){
+    modal.style.display="none";
+    document.querySelector('.modal img').src = "";
+        
+}
+
+
+// ****************************
+// const fotos = document.querySelectorAll('.container img')
+// const container2 = document.querySelector('.container') 
+// let ubicacion = 0;
+
+// container2.addEventListener('click', (e)=> {
+//     ubicacion = Array.from(fotos).indexOf(e.target)
+//     console.log(ubicacion)
+// })
+
+
+ 
