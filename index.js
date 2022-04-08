34f376f2b0150ca396c431099b0d940a9f91b21e
Damian Barrera <>
2022-04-08 19:17:46 -0300

    Boton de volver arriba

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 const names = document.querySelector('.names');
 const nombres = document.querySelectorAll('.datos');
 
- nombres.forEach( N => {
 
-     const link = document.createElement('a');
-     link.href = "#" + N.firstElementChild.textContent.toLowerCase().slice(1);
-     link.textContent = N.firstElementChild.textContent.slice(1);
-     
-     names.appendChild(link)
-    } )
+//  nombres.forEach( N => {
+//      const link = document.createElement('a');
+//       link.href = "#" + N.firstElementChild.textContent.toLowerCase().slice(1);
+//       link.textContent = N.firstElementChild.textContent.slice(1); 
+//       names.appendChild(link)
+//     } )
     
     // console.log(N.firstElementChild.textContent.toLowerCase().slice(1))
 
+    const dataName = [] ;
+    nombres.forEach( N => {
+        dataName.push(N.firstElementChild.textContent.toLowerCase().slice(1));             
+    })
+    
+    
+    dataName.sort().forEach ( DN => {
+        const link = document.createElement('a')
+        link.href = '#' + DN ;
+        link.textContent = DN;
+        names.appendChild(link)
+    })
 
-
-
-
-
-
-
+ 
