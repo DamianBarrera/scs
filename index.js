4d2f5f15ad23be0906bcc5e3296f7d60ff0768f9
Damian Barrera <>
2022-04-10 11:50:29 -0300

    Codigo ofuscado

--- index.js
+++ index.js
@@ -1,30 +1,22 @@
-const names = document.querySelector('.names');
-const nombres = document.querySelectorAll('.datos');
-const opciones = document.querySelector('.opciones');
+// const names = document.querySelector('.names');
+// const nombres = document.querySelectorAll('.datos');
+// const opciones = document.querySelector('.opciones');
 
-//  nombres.forEach( N => {
-//      const link = document.createElement('a');
-//       link.href = "#" + N.firstElementChild.textContent.toLowerCase().slice(1);
-//       link.textContent = N.firstElementChild.textContent.slice(1); 
-//       names.appendChild(link)
-//     } )
-    
-    // console.log(N.firstElementChild.textContent.toLowerCase().slice(1))
-
-    const dataName = [] ;
-    nombres.forEach( N => {
-        dataName.push(N.firstElementChild.textContent.toLowerCase().slice(1));             
-    })
+//     const dataName = [] ;
+//     nombres.forEach( N => {
+//         dataName.push(N.firstElementChild.textContent.toLowerCase().slice(1));             
+//     })
     
     
-    dataName.sort().forEach ( DN => {
-        const link = document.createElement('a')
-        link.href = '#' + DN ;
-        link.textContent = DN;
-        names.appendChild(link)
-    })
+//     dataName.sort().forEach ( DN => {
+//         const link = document.createElement('a')
+//         link.href = '#' + DN ;
+//         link.textContent = DN;
+//         names.appendChild(link)
+//     })
 
-   opciones.addEventListener('click',() => {
-        names.classList.toggle('show')
-   })
+//    opciones.addEventListener('click',() => {
+//         names.classList.toggle('show')
+//    })
  
+function _0x30fc(){const _0x49ab14=['querySelector','click','href','show','5619762tjSwmj','sort','classList','textContent','1MUUiJq','4ljwyXd','firstElementChild','1321976KTEeaE','8NjRzVC','2616815BXJQpx','310wGEMQs','251172LQFARY','createElement','forEach','2395953rUMxtW','.opciones','329384ZMTdAR','toggle','4537386GbEDPQ','toLowerCase'];_0x30fc=function(){return _0x49ab14;};return _0x30fc();}const _0x3bf782=_0x234f;(function(_0x2458b4,_0xcf23a2){const _0x510c98=_0x234f,_0x11cc59=_0x2458b4();while(!![]){try{const _0x347a4d=-parseInt(_0x510c98(0x1fa))/0x1*(-parseInt(_0x510c98(0x1e5))/0x2)+-parseInt(_0x510c98(0x1e9))/0x3+parseInt(_0x510c98(0x1fb))/0x4*(parseInt(_0x510c98(0x1e7))/0x5)+-parseInt(_0x510c98(0x1f0))/0x6+parseInt(_0x510c98(0x1ec))/0x7+parseInt(_0x510c98(0x1e6))/0x8*(parseInt(_0x510c98(0x1f6))/0x9)+-parseInt(_0x510c98(0x1e8))/0xa*(parseInt(_0x510c98(0x1ee))/0xb);if(_0x347a4d===_0xcf23a2)break;else _0x11cc59['push'](_0x11cc59['shift']());}catch(_0x3826a3){_0x11cc59['push'](_0x11cc59['shift']());}}}(_0x30fc,0x5d76d));const names=document[_0x3bf782(0x1f2)]('.names'),nombres=document['querySelectorAll']('.datos'),opciones=document['querySelector'](_0x3bf782(0x1ed)),dataName=[];function _0x234f(_0x52619b,_0x169071){const _0x30fcec=_0x30fc();return _0x234f=function(_0x234f26,_0x25afe7){_0x234f26=_0x234f26-0x1e5;let _0x4b7bb4=_0x30fcec[_0x234f26];return _0x4b7bb4;},_0x234f(_0x52619b,_0x169071);}nombres[_0x3bf782(0x1eb)](_0x4062f3=>{const _0x50abd8=_0x3bf782;dataName['push'](_0x4062f3[_0x50abd8(0x1fc)]['textContent'][_0x50abd8(0x1f1)]()['slice'](0x1));}),dataName[_0x3bf782(0x1f7)]()[_0x3bf782(0x1eb)](_0x50dd54=>{const _0x3662e9=_0x3bf782,_0x526dc4=document[_0x3662e9(0x1ea)]('a');_0x526dc4[_0x3662e9(0x1f4)]='#'+_0x50dd54,_0x526dc4[_0x3662e9(0x1f9)]=_0x50dd54,names['appendChild'](_0x526dc4);}),opciones['addEventListener'](_0x3bf782(0x1f3),()=>{const _0x3a4d5c=_0x3bf782;names[_0x3a4d5c(0x1f8)][_0x3a4d5c(0x1ef)](_0x3a4d5c(0x1f5));});
