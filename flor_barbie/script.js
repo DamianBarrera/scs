2165a944abb4b1a63dd6bb968311f56353a28e75
Damian Barrera <>
2022-04-15 16:19:09 -0300

    Agregadas fotos de Flor/barbie

--- flor_barbie/script.js
+++ flor_barbie/script.js
@@ -8,7 +8,10 @@ let contadorImg = undefined ;
 
 let flor = ["bar1.jpg","bar2.jpg","bar3.jpg","bar4.jpg","bar5.jpg","bar6.jpg","bar7.jpg","bar8.jpg","bar9.jpg"
 ,"bar10.jpg","bar11.jpg","bar12.jpg","bar13.jpg","bar14.jpg","bar15.jpg","bar16.jpg","bar17.jpg","bar18.jpg",
-"bar19.jpg","bar20.jpg","bar21.jpg","bar22.jpg","bar23.jpg"]
+"bar19.jpg","bar20.jpg","bar21.jpg","bar22.jpg","bar23.jpg","bar24.jpeg","bar25.jpeg"
+,"bar26.jpeg","bar26.jpeg","bar27.jpeg","bar28.jpeg","bar28.jpeg","bar29.jpeg","bar30.jpeg"
+,"bar31.jpeg","bar32.jpeg","bar33.jpeg","bar34.jpeg","bar35.jpeg","bar36.jpeg","bar37.jpeg"
+,"bar38.jpeg","bar39.jpeg","bar40.jpeg","bar41.jpeg"]
 
 
 
