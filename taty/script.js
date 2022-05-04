85021c6cc9a2cbefe12dd3ba94b71c24e4ac2b8e
Damian Barrera <>
2022-05-04 00:35:26 -0300

    agregada taty13

--- taty/script.js
+++ taty/script.js
@@ -18,7 +18,8 @@ const taty = [
     "https://github.com/DamianBarrera/imagenes/blob/main/imagenes/taty/taty6.jpg?raw=true",
     "https://github.com/DamianBarrera/imagenes/blob/main/imagenes/taty/taty7.jpg?raw=true",
     "https://github.com/DamianBarrera/imagenes/blob/main/imagenes/taty/taty8.jpg?raw=true",
-    "https://github.com/DamianBarrera/imagenes/blob/main/imagenes/taty/taty9.jpg?raw=true"
+    "https://github.com/DamianBarrera/imagenes/blob/main/imagenes/taty/taty9.jpg?raw=true",
+    "https://github.com/DamianBarrera/imagenes/blob/main/imagenes/taty/taty13.jpeg?raw=true"
 ]
 
 
